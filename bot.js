423801db240c67387732ec2e89e7bd6ab549ec99
iReBeLx <58609647+iReBeLx@users.noreply.github.com>
2020-05-13 04:06:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
    console.log(`----------------`);
       console.log(`Desert Bot- Script By : iReBeL`);
         console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : ABO WLEED' `);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : iReBeL' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`ABO WALEED`,"https://www.twitch.tv/abowaleed_")
